3097ccb1dc912e45eb00e119beb4ded20d56e6cf
David Counago Gonzalez <david.mouriscados@gmail.com>
2021-10-12 17:14:53 +0200

    Se ha creado un nuevo paquete con Subclase1 dentro del Paquete1

---
+++ src/Paquete1/Subpaquete1/Subclase1.java
@@ -0,0 +1,7 @@
+package Paquete1.Subpaquete1;
+
+public class Subclase1 {
+    public static void main(String[] args) {
+        System.out.println("Clase de un paquete dentro del Paquete1");
+    }
+}
